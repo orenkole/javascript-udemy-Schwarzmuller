a5ae9a3c06d78476705bce1949ece0d51957cf82
Oleh <orenkole@gmail.com>
2020-05-11 12:40:10 +0300

    move items between two lists

--- section14/assets/scripts/app.js
+++ section14/assets/scripts/app.js
@@ -81,41 +81,10 @@ class ProjectItem {
 
   constructor(id, updateProjectListsFunction, type) {
     this.id = id;
-    this.type = type;
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton(type);
     this.connectDrag();
-    this.connectDropable();
-  }
-
-  connectDropable() {
-    const list = document.querySelector(`#${this.type}-projects ul`);
-    list.addEventListener(
-      'dragenter',
-      event => {
-        if(event.dataTransfer.types[0] === "text/plain") {
-          event.preventDefault();
-        }
-        list.parentElement.classList.add('droppable');
-      }
-    )
-    list.addEventListener(
-      'dragover',
-      event => {
-        if(event.dataTransfer.types[0] === "text/plain") {
-          event.preventDefault();
-        }
-      }
-    )
-    list.addEventListener(
-      'dragLeave',
-      event => {
-        if(event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
-          list.parentElement.classList.remove('droppable');
-        }
-      }
-    )
   }
 
   showMoreInfoHandler() {
@@ -136,13 +105,15 @@ class ProjectItem {
   }
 
   connectDrag() {
-    document.getElementById(this.id).addEventListener(
-      'dragstart',
-      event => {
-        event.dataTransfer.setData("text/plain", this.id);
-        event.dataTransfer.effectAllowed = 'move';
-      }
-    )
+    const item = document.getElementById(this.id);
+    item.addEventListener('dragstart', event => {
+      event.dataTransfer.setData('text/plain', this.id);
+      event.dataTransfer.effectAllowed = 'move';
+    });
+
+    item.addEventListener('dragend', event => {
+      console.log(event);
+    });
   }
 
   connectMoreInfoButton() {
@@ -182,6 +153,43 @@ class ProjectList {
       );
     }
     console.log(this.projects);
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+
+    list.addEventListener('dragenter', event => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        list.parentElement.classList.add('droppable');
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener('dragover', event => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener('dragleave', event => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove('droppable');
+      }
+    });
+
+    list.addEventListener('drop', event => {
+      const prjId = event.dataTransfer.getData('text/plain');
+      if (this.projects.find(p => p.id === prjId)) {
+        return;
+      }
+      document
+        .getElementById(prjId)
+        .querySelector('button:last-of-type')
+        .click();
+      list.parentElement.classList.remove('droppable');
+      // event.preventDefault(); // not required
+    });
   }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
