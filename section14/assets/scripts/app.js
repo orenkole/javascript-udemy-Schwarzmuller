52553ea663ec88a63b37b98df303452938ff1888
Oleh <orenkole@gmail.com>
2020-05-11 14:24:15 +0300

    project: grag and drop implemented

--- section14/assets/scripts/app.js
+++ section14/assets/scripts/app.js
@@ -220,6 +220,19 @@ class App {
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
     );
+
+    // const timerId = setTimeout(this.startAnalytics, 3000);
+
+    // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+    //   clearTimeout(timerId);
+    // });
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = 'assets/scripts/analytics.js';
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
   }
 }
 
