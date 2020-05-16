40baee73d198e981be0e1d0b8c7baa14c784734e
Oleh <orenkole@gmail.com>
2020-05-16 10:58:01 +0300

    added http request execution through UI elements

--- section18/assets/scripts/app.js
+++ section18/assets/scripts/app.js
@@ -1,5 +1,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
 
 function sendHttpRequest(method, url, data = null) {
   const promise = new Promise((resolve, reject) => {
@@ -43,5 +45,10 @@ async function createPost(title, content) {
   )
 }
 
-fetchPosts();
-createPost('POST', 'A dummy post')
+fetchButton.addEventListener('click', () => fetchPosts);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
+  const enteredContent = event.currentTarget.querySelector('#content').value;
+  createPost(enteredTitle, enteredContent);
+})
