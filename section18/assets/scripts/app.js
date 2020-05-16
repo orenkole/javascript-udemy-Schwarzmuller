ea34b124d4562627fb39a37e2b797bb93d3fc94f
Oleh <orenkole@gmail.com>
2020-05-16 12:16:32 +0300

    added http request to delete posts and replaced XMLHttpRequest with fetch()

--- section18/assets/scripts/app.js
+++ section18/assets/scripts/app.js
@@ -2,34 +2,48 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
-function sendHttpRequest(method, url, data = null) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      resolve(xhr.response);
-    }
-    xhr.send(JSON.stringify(data));
-  })
-  return promise;
+function sendHttpRequest(url) {
+  // const promise = new Promise((resolve, reject) => {
+    // const xhr = new XMLHttpRequest();
+    // xhr.open(method, url);
+    // xhr.responseType = 'json';
+    // xhr.onload = function() {
+    //   if(xhr.status >= 200 && xhr.status < 300) {
+    //     resolve(xhr.response);
+    //   } else {
+    //     reject(new Error('Something went wrong'))
+    //   }
+    // }
+    // xhr.onerror = function() {
+    //   reject(new Error('Failed to send request'))
+    // }
+    // xhr.send(JSON.stringify(data));
+  // })
+  // return promise;
+  return fetch(url).then(response => {
+    return response.;
+  });
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  )
-  for(const post of responseData) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts'
+    )
+    for(const post of responseData) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch(err) {
+    alert(err);
   }
 }
-
-fetchPosts();
 
 async function createPost(title, content) {
   const userId = Math.random();
@@ -45,10 +59,20 @@ async function createPost(title, content) {
   )
 }
 
-fetchButton.addEventListener('click', () => fetchPosts);
+fetchButton.addEventListener('click', fetchPosts);
 form.addEventListener('submit', event => {
   event.preventDefault();
   const enteredTitle = event.currentTarget.querySelector('#title').value;
   const enteredContent = event.currentTarget.querySelector('#content').value;
   createPost(enteredTitle, enteredContent);
+})
+
+postList.addEventListener('click', event => {
+  if(event.target.tagName === 'button') {
+    const postId = event.target.closest('li').id;
+    sendHttpRequest(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    )
+  }
 })
